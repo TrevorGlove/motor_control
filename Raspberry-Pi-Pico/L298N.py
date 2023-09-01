b6f44a1a316fa260daaccd822ba424fab37bf40d
Robêrt <132081786+TrevorGlove@users.noreply.github.com>
2023-09-01 12:06:11 -0500

    Create L298N.py

---
+++ Raspberry-Pi-Pico/L298N.py
@@ -0,0 +1,25 @@
+from machine import Pin, PWM
+
+class L298N:
+    def __init__(self, in1_pin, in2_pin, ena_pin, motor_num):
+        self.in1 = Pin(in1_pin, Pin.OUT)
+        self.ena = Pin(ena_pin, Pin.OUT)
+        
+        self.motor_num = motor_num
+        self.pwm = PWM(self.ena)
+        
+        self.speed(0)
+    
+    def speed(self, value):
+        
+        if value < 0:
+            self.in1.off()
+            self.in2.on()
+        else:
+            self.in1.on()
+            self.in2.off()
+            
+        value = abs(value)  
+        self.pwm.freq(1000)  
+        self.pwm.duty_u16(value)
+      
