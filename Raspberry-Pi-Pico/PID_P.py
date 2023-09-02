b85009fb0789d2573651dc169a8a0c25cbefe59e
Robêrt <132081786+TrevorGlove@users.noreply.github.com>
2023-09-02 15:54:39 -0500

    Update PID_P.py

--- Raspberry-Pi-Pico/PID_P.py
+++ Raspberry-Pi-Pico/PID_P.py
@@ -2,7 +2,6 @@
 
 from machine import Pin, ADC, Timer
 from L298N import L298N
-from time import sleep
 
 # -----Declaración de pines----
 
@@ -18,17 +17,17 @@ PinPOT = 26
 contador = 0   # Pulsos de encoder
 pv = 486       # Total de pulsos por vuelta
 
-vA1 = 0        # vA(k-1) 
+vA1 = 0        # vA(k-1)
 vB1 = 0        # vB(k-1)
 
 dirmotor = False  # F dirección hacia atras y V dirección hacia adelante
-gr = 0         # Grados de giro del motor 
+gr = 0         # Grados de giro del motor
 
 # PID
 sp = 0
-c = [0.0, 0.0, 0.0]        # [c, c(k-1), c(k-2)] 
+c = [0.0, 0.0, 0.0]        # [c, c(k-1), c(k-2)]
 e = [0.0, 0.0, 0.0]        # [e, e(k-1), e(k-2)]
-K = [1.0, 2.2, 0.3]       # [Kp, Ki, Kd]
+K = [5.902, 4.68, 0.00785]       # [Kp, Ki, Kd]
 Ts = 0.1
 
 q = [K[0] + K[2]/Ts, -K[0] + K[1]*Ts - 2*K[2]/Ts, K[2]/Ts]
@@ -40,7 +39,7 @@ q = [K[0] + K[2]/Ts, -K[0] + K[1]*Ts - 2*K[2]/Ts, K[2]/Ts]
 
 def interrupcion(pin):
     global contador, vA, vA1, vA2, vB, dirmotor
-    vA = A.value()  # Valor de Pin A de encoder 
+    vA = A.value()  # Valor de Pin A de encoder
     if not vA1 and vA:
         vB = B.value()  # Valor de Pin B de encoder
         if vB == (False and dirmotor):
@@ -48,26 +47,27 @@ def interrupcion(pin):
         elif vB == (True and (not dirmotor)):
             dirmotor = True
     vA1 = vA
-    
+
     contador += 1 if not dirmotor else -1
-        
+
 def PID(timer):
     global gr, contador, pot, sp, c, e, pv, Ts, q
-    
-    gr = int(contador*360/pv)
-    
-    pot = adc.read_u16()
-    sp = int(pot*180/65535 - 90)
+
+    gr = contador*360/pv
+
+    sp = pot*360/65535 - 180
     e[0] = sp - gr
-    
+
     # -----Control PID----
-    
+
     c[0] = c[1] + q[0]*e[0] + q[1]*e[1] + q[2]*e[2]
-    
+
     c[1] = c[0]
     c[2] = c[1]
     e[1] = e[0]
     e[2] = e[1]
+    
+    print(("Sp", int(sp), "Gr", int(gr)))
 
 # -----Configuración de pines----
 
@@ -87,9 +87,9 @@ tim = Timer()
 tim.init(period=int(Ts * 1000), mode=Timer.PERIODIC, callback=PID)
 
 while True:
+    
+    pot = adc.read_u16()
     c = [min(max(value, -500), 500) for value in c]
- 
     motor1.speed(int(c[0]*65535/500))
-    # print("Sp: ", sp, "Gr: ", gr)
-    print((sp, gr))
-    sleep(0.05)
+
+
